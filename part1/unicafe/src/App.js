c03cb9970cbaff74faa9f673eb935052bcc4b4e9
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-13 14:52:26 +0400

    unicafe 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,32 +4,46 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value} {text === 'positive' ? '%' : ''}</p>
+const TableRow = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value} { text === 'positive' ? '%' : ''}</td>
+  </tr>
+)
+
+const DisplayTable = ({good, neutral, bad, all, average, positive}) => (
+  <table>
+    <tbody>
+      <TableRow text='good' value={ good } />
+      <TableRow text='neutral' value={ neutral } />
+      <TableRow text='bad' value={ bad } />
+      <TableRow text='all' value={ all } />
+      <TableRow text='average' value={ average } />
+      <TableRow text='positive' value={ positive } />
+    </tbody>
+  </table>
+)
 
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   const average = (good - bad) / all
   const positive = (good / all) * 100
 
-  if (good || neutral || bad) {
+  if (good || neutral || bad) { 
     return (
       <div>
-        <StatisticLine text='good' value={ good } />
-        <StatisticLine text='neutral' value={ neutral } />
-        <StatisticLine text='bad' value={ bad } />
-        <StatisticLine text='all' value={ all } />
-        <StatisticLine text='average' value={ average } />
-        <StatisticLine text='positive' value={ positive } />
-      </div>
+        <DisplayTable 
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={all}
+          average={average}
+          positive={positive} />
+      </div> 
     )
   }
-  else {
-    return (
-      <div>No feedback given</div> 
-    )
-  }
+  else { return <div>No feedback given</div> }
 }
-
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -38,10 +52,10 @@ const App = () => {
 
   return (
     <div>
-      <Header text='give feedback'/>
-      <Button text='good' handleClick={() => setGood(good + 1)}/>
-      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)}/>
-      <Button text='bad' handleClick={() => setBad(bad + 1)}/>
+      <Header text='give feedback' />
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
 
       <Header text='statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} />
