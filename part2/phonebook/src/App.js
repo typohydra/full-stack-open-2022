a05699ce712d316ae27def4f799f67b10a7ee22d
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-15 12:36:53 +0400

    phonebook 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFileter, setNewFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const checkIfNameExists = (name) => {
     return persons.find( person => person.name === name)
@@ -36,12 +41,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFileterChange = (event) => {
+    setNewFilter(event.target.value)
+    setShowAll(false)
+  }
+
+  const peopleToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFileter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFileter} onChange={handleFileterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           number: <input value={newNumber} onChange={handleNumberChange} />
@@ -52,7 +70,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {peopleToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
