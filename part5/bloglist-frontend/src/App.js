81c6226d19fe1ba92afc00565c76468bab7462a0
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-30 18:52:59 +0400

    bloglist frontend 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,14 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBologAppUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
   
   const handleUsernameChange = ({target}) => setUsername(target.value)
   const handlePasswordChange = ({target}) => setPassword(target.value)
@@ -22,11 +30,21 @@ const App = () => {
     event.preventDefault()
     try {
       const user = await loginService.login({username, password})
+      window.localStorage.setItem(
+        'loggedBologAppUser', JSON.stringify(user)
+      )
       setUser(user)
+      setUsername('')
+      setPassword('')
     }
     catch (exception) {
       console.log(exception);
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   if(user === null) {
@@ -46,7 +64,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      {user.name} logged in
+      <button type='button' onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
