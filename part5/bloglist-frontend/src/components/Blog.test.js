7651dc34aca574af55c9cc82f3d241ccbf249a07
typohydra <74554736+typohydra@users.noreply.github.com>
2022-08-02 16:46:24 +0400

    blog list tests 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('blog component only renders title and author', () => {
+let container
+
+beforeEach(() => {
   const loggedUser = {
     username: 'testing-username'
   }
@@ -19,11 +22,23 @@ test('blog component only renders title and author', () => {
     }
   }
 
-  const { container } = render(<Blog blog={blog} loggedUser={loggedUser} />)
+  container = render(<Blog blog={blog} loggedUser={loggedUser} />).container
+})
 
+test('blog component only renders title and author', () => {
   const div1 = container.querySelector('.title-author')
   const div2 = container.querySelector('.title-author-url-likes')
 
   expect(div1).not.toHaveStyle('display: none')
   expect(div2).toHaveStyle('display: none')
+})
+
+
+test('blog component renders url and likes after button click', async () => {
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const div = container.querySelector('.title-author-url-likes')
+  expect(div).not.toHaveStyle('display: none')
 })
