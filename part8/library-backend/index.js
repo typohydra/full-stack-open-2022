c7566a271db6eecc9208cbb643b1e6b08698bbad
typohydra <74554736+typohydra@users.noreply.github.com>
2022-08-29 11:36:14 +0400

    all books 8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -26,12 +26,6 @@ let authors = [
   },
 ]
 
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
 let books = [
   {
     title: 'Clean Code',
@@ -85,16 +79,25 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    genres: [String!]!
+    published: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
