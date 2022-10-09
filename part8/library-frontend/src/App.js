657a4e310841a5e6459423678f8f93cb51864d5e
typohydra <74554736+typohydra@users.noreply.github.com>
2022-10-09 18:28:56 +0400

    log in 8.18

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,12 +1,24 @@
 import { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Notify from './components/Notify'
+import LoginForm from './components/LoginForm' 
+import { useEffect } from 'react'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [page, setPage] = useState('authors')
   const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
+
+  useEffect(() => {
+    const userToken =  localStorage.getItem('library-user-token');
+    if ( userToken ) {
+      setToken(userToken)
+    }
+  }, [])
 
   const notify = (message) => {
     setErrorMessage(message)
@@ -15,13 +27,39 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Notify errorMessage={errorMessage} />
+
+        <Authors show={page === 'authors'} setError={notify} />
+
+        <Books show={page === 'books'} />
+
+        <LoginForm show={page === 'login'} setError={notify} setToken={setToken} setPage={setPage}/>
+      </div>
+    )
+  }
+
   return (
     <div>
-
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
 
       <Notify errorMessage={errorMessage} />
