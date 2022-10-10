4e78418a2f05bb19028a9325c0a97c4e78a1940a
typohydra <74554736+typohydra@users.noreply.github.com>
2022-10-10 15:36:21 +0400

    books by genre 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,24 +1,40 @@
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
+import { useState } from 'react'
 
 const Books = ({show}) => {
+  const [filter, setFilter] = useState('')
   const result = useQuery(ALL_BOOKS, {
-    skip: !show,
+    skip: !show
+  })
+  const genreResult = useQuery(ALL_BOOKS, {
+    skip: !show && !filter,
+    variables: { genre: filter }
   })
 
   if (!show) {
     return null
   }
 
-  if (result.loading) {
+  if (result.loading || genreResult.loading) {
     return <div>loading...</div>;
   }
   
   const books = result.data.allBooks
+  // for buttons
+  const uniqueGenres = [...new Set(books.map(book => book.genres).flat(1))]
+  // for list display
+  const genreBooks = genreResult.data.allBooks
+  
+  const handleGenreChange = (genre) => {
+    setFilter(genre)
+  }
 
   return (
     <div>
       <h2>books</h2>
+
+      <p>in genre <b>{filter ? filter : 'all genres'}</b></p>
 
       <table>
         <tbody>
@@ -27,7 +43,7 @@ const Books = ({show}) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {genreBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -36,6 +52,13 @@ const Books = ({show}) => {
           ))}
         </tbody>
       </table>
+      
+      <div>
+        {uniqueGenres.map(genre => (
+          <button key={genre} onClick={() => handleGenreChange(genre)}>{genre}</button>
+        ))}
+        <button onClick={() => handleGenreChange('')}>all genres</button>
+      </div>
     </div>
   )
 }
